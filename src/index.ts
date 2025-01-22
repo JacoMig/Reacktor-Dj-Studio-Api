fa97b5e5839e521500740f32eb01644f4338eed4
JacoMig <jacomig@gmail.com>
2025-01-22 15:40:36 +0100

    feat: add render host and local env

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import fastify from "fastify";
 import cors from '@fastify/cors'
+
+const HOST = process.env.HOST || '0.0.0.0'
+
 const app = fastify({
     logger: true
 })
 app.register(import('./routes'))
 app.register(import('./plugins/s3Client'))
 app.register(cors)
-app.listen({host: 'localhost', port: 2000 }, (err) => {
+app.listen({host: HOST, port: 2000 }, (err) => {
     if (err) {
         app.log.error(err)
         process.exit(1)
